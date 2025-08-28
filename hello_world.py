6d82121cf87a6997ef2b7c1eabbddac2e1d8e429
FluffyGiraffe09 <oroy2028@kehillahstudent.org>
2025-08-28 09:14:30 -0700

    I found some errors

---
+++ hello_world.py
@@ -0,0 +1,51 @@
+print(9%2)
+#       (cs2526) scottroy@Scotts-MacBook-Air 01-hello-world-and-error-hunt-FluffyGiraffe09 % /opt/miniconda3/envs/cs2526/bin/python /Users/scottroy/github-classroo
+# m/tkscs/01-hello-world-and-error-hunt-FluffyGiraffe09/hello_world.py
+#   File "/Users/scottroy/github-classroom/tkscs/01-hello-world-and-error-hunt-FluffyGiraffe09/hello_world.py", line 1
+#     print("My name is ORLI:))
+#           ^
+# SyntaxError: unterminated string literal (detected at line 1)
+
+# (cs2526) scottroy@Scotts-MacBook-Air 01-hello-world-and-error-hunt-FluffyGiraffe09 % /opt/miniconda3/envs/cs2526/bin/python /Users/scottroy/github-classroo
+# m/tkscs/01-hello-world-and-error-hunt-FluffyGiraffe09/hello_world.py
+#   File "/Users/scottroy/github-classroom/tkscs/01-hello-world-and-error-hunt-FluffyGiraffe09/hello_world.py", line 1
+#     print("My name is ORLI:)"
+#          ^
+# SyntaxError: '(' was never closed
+
+# (cs2526) scottroy@Scotts-MacBook-Air 01-hello-world-and-error-hunt-FluffyGiraffe09 % /opt/miniconda3/envs/cs2526/bin/python /Users/scottroy/github-classroo
+# m/tkscs/01-hello-world-and-error-hunt-FluffyGiraffe09/hello_world.py
+#   File "/Users/scottroy/github-classroom/tkscs/01-hello-world-and-error-hunt-FluffyGiraffe09/hello_world.py", line 1
+#     print My name is ORLI:
+#     ^^^^^^^^
+# SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+
+
+# (cs2526) scottroy@Scotts-MacBook-Air 01-hello-world-and-error-hunt-FluffyGiraffe09 % /opt/miniconda3/envs/cs2526/bin/python /Users/scottroy/github-classroo
+# m/tkscs/01-hello-world-and-error-hunt-FluffyGiraffe09/hello_world.py
+# Traceback (most recent call last):
+#   File "/Users/scottroy/github-classroom/tkscs/01-hello-world-and-error-hunt-FluffyGiraffe09/hello_world.py", line 1, in <module>
+#     prin ("Hello World")
+#     ^^^^
+# NameError: name 'prin' is not defined. Did you mean: 'print'?
+
+# (cs2526) scottroy@Scotts-MacBook-Air 01-hello-world-and-error-hunt-FluffyGiraffe09 % /opt/miniconda3/envs/cs2526/bin/python /Users/scottroy/github-classroo
+# m/tkscs/01-hello-world-and-error-hunt-FluffyGiraffe09/hello_world.py
+#   File "/Users/scottroy/github-classroom/tkscs/01-hello-world-and-error-hunt-FluffyGiraffe09/hello_world.py", line 1
+#     print(""HelloWorld"")
+#           ^^^^^^^^^^^^
+# SyntaxError: invalid syntax. Perhaps you forgot a comma?
+
+# (cs2526) scottroy@Scotts-MacBook-Air 01-hello-world-and-error-hunt-FluffyGiraffe09 % /opt/miniconda3/envs/cs2526/bin/python /Users/scottroy/github-classroo
+# m/tkscs/01-hello-world-and-error-hunt-FluffyGiraffe09/hello_world.py
+#   File "/Users/scottroy/github-classroom/tkscs/01-hello-world-and-error-hunt-FluffyGiraffe09/hello_world.py", line 1
+#     print("HelloWorld")
+# IndentationError: unexpected indent
+
+# (cs2526) scottroy@Scotts-MacBook-Air 01-hello-world-and-error-hunt-FluffyGiraffe09 % /opt/miniconda3/envs/cs2526/bin/python /Users/scottroy/github-classroo
+# m/tkscs/01-hello-world-and-error-hunt-FluffyGiraffe09/hello_world.py
+# Traceback (most recent call last):
+#   File "/Users/scottroy/github-classroom/tkscs/01-hello-world-and-error-hunt-FluffyGiraffe09/hello_world.py", line 1, in <module>
+#     print["hi"]
+#     ~~~~~^^^^^^
+# TypeError: 'builtin_function_or_method' object is not subscriptable
